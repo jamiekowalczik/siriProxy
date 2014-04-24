6c8a8be2335c89e39ce87fbc76f08c142b6212c5
jamiekowalczik <jamie.kowalczik@gmail.com>
2014-04-24 09:25:52 -0400

    Update emailimg.sh

--- emailimg.sh
+++ emailimg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget "http://192.168.2.123/img/snapshot.cgi?size=3&quality=1" -O webcam.jpg >> /dev/null 2>&1
+wget "http://1.1.1.1/img/snapshot.cgi?size=3&quality=1" -O webcam.jpg >> /dev/null 2>&1
 /usr/bin/email -t someemail@here.com -f someemail@here.com -a "webcam.jpg" -s "Basement Camera" -b "Basement Camera" >> /dev/null 2>&1
 rm webcam.jpg >> /dev/null 2>&1
 echo "Image Successfully Emailed"
